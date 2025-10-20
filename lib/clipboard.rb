b36daa98a47ee0b7fca99fe823743da8f9e8f08d
Radoslav Stankov <rstankov@gmail.com>
2025-10-20 22:40:49 +0300

    Clipboard fix

--- lib/clipboard.rb
+++ lib/clipboard.rb
@@ -5,13 +5,17 @@ module PromptBox
     extend self
 
     def read
-      `osascript -e 'the clipboard as Unicode text'`
+      IO.popen(['pbpaste'], &:read).to_s
+    rescue Errno::ENOENT
+      raise 'pbpaste command not available'
     end
 
     def write(text)
-      escaped_text = text.gsub('\\', '\\\\\\\\').gsub('"', '\\\"').gsub("\n", '\\\\n')
-
-      `osascript -e 'set the clipboard to "#{escaped_text}"'`
+      IO.popen(['pbcopy'], 'w') { |io| io.write(text.to_s) }
+      status = $?
+      raise 'pbcopy command failed' if status && !status.success?
+    rescue Errno::ENOENT
+      raise 'pbcopy command not available'
     end
   end
 end
