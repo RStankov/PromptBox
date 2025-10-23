30c8d98cba506c43534033adedb757bd56112fec
Radoslav Stankov <rstankov@gmail.com>
2025-10-23 23:18:14 +0300

    Bundle terminal notifier

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -4,18 +4,27 @@ require 'securerandom'
 
 module PromptBox
   class Notifier
+    BUNDLED_NOTIFIER = File.expand_path('../../vendor/terminal-notifier.app/Contents/MacOS/terminal-notifier', __dir__)
+
+    def self.binary_path
+      return BUNDLED_NOTIFIER if File.exist?(BUNDLED_NOTIFIER)
+
+      'terminal-notifier'
+    end
+
     def initialize(title:)
       @title = title
       @group = "prompt-box-#{SecureRandom.hex(6)}"
+      @binary = self.class.binary_path
     end
 
     def show(message)
-      `terminal-notifier -title "#{@title}" -message "#{message}" -group "#{@group}" -sender "com.runningwithcrayons.Alfred"`
+      `#{@binary} -title "#{@title}" -message "#{message}" -group "#{@group}" -sender "com.runningwithcrayons.Alfred"`
     end
 
     def close(after:)
       sleep(after)
-      `terminal-notifier -remove '#{@group}' > /dev/null`
+      `#{@binary} -remove '#{@group}' > /dev/null`
     end
   end
 end
